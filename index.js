c700b480de33df41efed0b9d861526f109b2a9ff
Raphael Itotia <raphael1itotia@gmail.com>
2022-08-16 18:22:13 +0300

    2 passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.body.removeChild(main);
+
+const newHeader = document.createElement('h1');
+document.body.append(newHeader);
+newHeader.id("victory");
+newHeader.textContent = 'Harry is the champion.';
+
+
+
+
